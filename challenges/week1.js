4293a1943480f1035d7279e1eacdd056deecfa91
Jo Preihs <jo_preihs@hotmail.com>
2019-03-24 12:43:30 +0000

    Week 1 Tests - Q3

--- challenges/week1.js
+++ challenges/week1.js
@@ -15,6 +15,7 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
+  else return originalPrice + ((originalPrice * vatRate) / 100) ;
   // Add your code here!
 }
 
--- test/week1.test.js
+++ test/week1.test.js
@@ -32,7 +32,7 @@ describe("generateInitials", () => {
 });
 
 describe("addVAT", () => {
-  test.only("adds a VAT of 20% to a price of 100", () => {
+  test("adds a VAT of 20% to a price of 100", () => {
     expect(addVAT(100, 20)).toBe(120);
   });
 
@@ -40,8 +40,8 @@ describe("addVAT", () => {
     expect(addVAT(40, 17.5)).toBe(47);
   });
 
-  test("adds a VAT of 17.5% to a price of 33.50", () => {
-    expect(addVAT(33.5, 17.5)).toBe(39.36);
+  test.only("adds a VAT of 17.5% to a price of 33.50", () => {
+    expect(addVAT(33.5, 17.5)).toBe(39.3625);
   });
 
   test("adds a VAT of 0% to a price of 25", () => {
