196625384a410e93dea80e44e0749dc3f0916d53
Jo Preihs <jo_preihs@hotmail.com>
2019-05-14 21:29:14 +0100

    Week 8 JS Challenges - In progress

--- challenges/week8.js
+++ challenges/week8.js
@@ -1,8 +1,19 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  
+  for (let i = 0; i < nums.length; i++){
+  const num = nums[i];
+  const givenNum = n;
+  if (num[i] === givenNum) {
+   const nextNum = num.getNext();
+   return nextNum;
+  } else {
+    return null;
+  }
+  }
 };
+//NOT WORKING YET
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
@@ -11,8 +22,10 @@ const count1sand0s = str => {
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
-};
+ const newNumber = (n.toString().split('').reverse().join('')) * Math.sign(n);
+ return newNumber;
+}
+
 
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
